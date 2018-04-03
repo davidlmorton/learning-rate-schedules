aac0f67d1efa5da80b0ad2ff27e4556ce0b7293e
David Morton <davidlmorton@gmail.com>
2018-04-03 13:12:12 -0500

    Add ability to run grid search

---
+++ loop-lr-only.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for j in `seq 1 3`; do
+    for i in `seq 1 9`; do
+        for sched in piecewise-constant; do
+           lr=${i}e-${j}
+           echo ${sched}-${lr}
+           tox -e python -- run_single_experiment.py -l ${lr} -d lr-only -y basic_classifier_MNIST.yaml -r ${sched}
+        done
+    done
+done
+
+for lr in 1.0 1.1 1.2 1.3 1.4 1.5 1.6; do
+    for sched in piecewise-constant; do
+       echo ${sched}-${lr}
+       tox -e python -- run_single_experiment.py -l ${lr} -d lr-only -y basic_classifier_MNIST.yaml -r ${sched}
+    done
+done
---
+++ run_single_experiment.py
@@ -0,0 +1,100 @@
+import matplotlib
+matplotlib.use('Agg')
+
+from sconce.data_generator import DataGenerator
+from sconce.models import BasicClassifier
+from sconce import rate_controllers
+from sconce.trainers import ClassifierTrainer
+from torch import optim
+from torchvision import datasets
+
+import click
+import csv
+import os
+import random
+import sconce
+import torch
+import uuid
+
+
+@click.command()
+@click.option('--initial-learning-rate', '-l', type=click.FLOAT, required=True)
+@click.option('--experiment-directory', '-d', type=click.Path(), required=True)
+@click.option('--yaml-file', '-y', type=click.File(), required=True)
+@click.option('--rate-schedule', '-r',
+        type=click.Choice(['piecewise-constant', 'clr-cosine',
+            'clr-triangle', 'clr-step']),
+        required=True)
+def single(initial_learning_rate, experiment_directory,
+        yaml_file, rate_schedule):
+    os.makedirs(experiment_directory, exist_ok=True)
+
+    model = BasicClassifier.new_from_yaml_file(yaml_file)
+
+    training_generator = DataGenerator.from_pytorch(
+            dataset_class=datasets.FashionMNIST)
+    test_generator = DataGenerator.from_pytorch(
+            dataset_class=datasets.FashionMNIST,
+            train=False)
+
+    if torch.cuda.is_available():
+        model.cuda()
+        training_generator.cuda()
+        test_generator.cuda()
+
+    optimizer = optim.SGD(model.parameters(), lr=initial_learning_rate,
+            momentum=0.9, weight_decay=1e-4)
+
+    trainer = ClassifierTrainer(model=model, optimizer=optimizer,
+        training_data_generator=training_generator,
+        test_data_generator=test_generator)
+
+    rcs = {
+        'clr-cosine': rate_controllers.CosineRateController(
+            max_learning_rate=initial_learning_rate,
+            min_learning_rate=initial_learning_rate/50),
+        'clr-triangle': rate_controllers.TriangleRateController(
+            max_learning_rate=initial_learning_rate,
+            min_learning_rate=initial_learning_rate/50),
+        'clr-step': rate_controllers.StepRateController(
+            max_learning_rate=initial_learning_rate,
+            min_learning_rate=initial_learning_rate/50,
+            num_drops=1),
+        'piecewise-constant': rate_controllers.ConstantRateController(
+            learning_rate=initial_learning_rate,
+            num_drops=3, movement_window=800, movement_threshold=0.20),
+    }
+    rate_controller = rcs[rate_schedule]
+
+    if 'clr' in rate_schedule:
+        trainer.multi_train(num_cycles=10, cycle_multiplier=1.5,
+            rate_controller=rate_controller)
+    else:
+        trainer.train(num_epochs=250, rate_controller=rate_controller)
+
+    fig = trainer.monitor.dataframe_monitor.plot(skip_first=100,
+        smooth_window=30, metrics=['classification_accuracy', 'loss'])
+    unique_part = str(uuid.uuid4())[-5:]
+    name = f'{rate_schedule}-{initial_learning_rate}-{unique_part}'
+    fig.savefig(os.path.join(experiment_directory,
+        f'{name}-training-history.png'))
+
+    monitor_file = os.path.join(experiment_directory, 'monitors.hd5')
+    trainer.monitor.dataframe_monitor.save(monitor_file, key=name)
+
+    acc = trainer.get_classification_accuracy()
+    print(f"Classification Accuracy: {acc}")
+
+    csv_file = os.path.join(experiment_directory, 'results.csv')
+    write_header = not os.path.exists(csv_file)
+
+    with open(os.path.join(experiment_directory, 'results.csv'), 'a') \
+            as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        if write_header:
+            writer.writerow(['rate_schedule', 'initial_learning_rate', 'acc'])
+        writer.writerow([rate_schedule, initial_learning_rate, acc])
+
+
+if __name__ == '__main__':
+    single()
